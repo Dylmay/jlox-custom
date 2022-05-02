2813d9041e72aa5324785baef431149eefd66229
Dylan Mayor <dylmay456@icloud.com>
2022-05-02 12:29:16 +0100

    Added Expression class & generator

---
+++ src/main/java/com/dylmay/JLox/Assets/Expr.java
@@ -0,0 +1,218 @@
+package com.dylmay.JLox.Assets;
+
+import javax.annotation.Nullable;
+
+public abstract class Expr {
+  public interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+
+    R visitTernaryExpr(Ternary expr);
+
+    R visitGroupingExpr(Grouping expr);
+
+    @Nullable
+    R visitLiteralExpr(Literal expr);
+
+    R visitUnaryExpr(Unary expr);
+  }
+
+  public abstract <R> R accept(Visitor<R> visitor);
+
+  public static class Binary extends Expr {
+    public final Expr left;
+    public final Token operator;
+    public final Expr right;
+
+    public Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+      if (this == obj) return true;
+
+      if (obj instanceof Binary i) {
+        return this.left != null
+            && this.left.equals(i.left)
+            && this.operator != null
+            && this.operator.equals(i.operator)
+            && this.right != null
+            && this.right.equals(i.right);
+      }
+
+      return false;
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+
+      result = prime * result + ((left == null) ? 0 : left.hashCode());
+      result = prime * result + ((operator == null) ? 0 : operator.hashCode());
+      result = prime * result + ((right == null) ? 0 : right.hashCode());
+
+      return result;
+    }
+  }
+
+  public static class Ternary extends Expr {
+    public final Expr condition;
+    public final Expr onTrue;
+    public final Expr onFalse;
+
+    public Ternary(Expr condition, Expr onTrue, Expr onFalse) {
+      this.condition = condition;
+      this.onTrue = onTrue;
+      this.onFalse = onFalse;
+    }
+
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitTernaryExpr(this);
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+      if (this == obj) return true;
+
+      if (obj instanceof Ternary i) {
+        return this.condition != null
+            && this.condition.equals(i.condition)
+            && this.onTrue != null
+            && this.onTrue.equals(i.onTrue)
+            && this.onFalse != null
+            && this.onFalse.equals(i.onFalse);
+      }
+
+      return false;
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+
+      result = prime * result + ((condition == null) ? 0 : condition.hashCode());
+      result = prime * result + ((onTrue == null) ? 0 : onTrue.hashCode());
+      result = prime * result + ((onFalse == null) ? 0 : onFalse.hashCode());
+
+      return result;
+    }
+  }
+
+  public static class Grouping extends Expr {
+    public final Expr expression;
+
+    public Grouping(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+      if (this == obj) return true;
+
+      if (obj instanceof Grouping i) {
+        return this.expression != null && this.expression.equals(i.expression);
+      }
+
+      return false;
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+
+      result = prime * result + ((expression == null) ? 0 : expression.hashCode());
+
+      return result;
+    }
+  }
+
+  public static class Literal extends Expr {
+    public final @Nullable Object value;
+
+    public Literal(@Nullable Object value) {
+      this.value = value;
+    }
+
+    @Override
+    public <R> @Nullable R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+      if (this == obj) return true;
+
+      if (obj instanceof Literal i) {
+        return this.value != null && this.value.equals(i.value);
+      }
+
+      return false;
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+
+      result = prime * result + ((value == null) ? 0 : value.hashCode());
+
+      return result;
+    }
+  }
+
+  public static class Unary extends Expr {
+    public final Token operator;
+    public final Expr right;
+
+    public Unary(Token operator, Expr right) {
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+      if (this == obj) return true;
+
+      if (obj instanceof Unary i) {
+        return this.operator != null
+            && this.operator.equals(i.operator)
+            && this.right != null
+            && this.right.equals(i.right);
+      }
+
+      return false;
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+
+      result = prime * result + ((operator == null) ? 0 : operator.hashCode());
+      result = prime * result + ((right == null) ? 0 : right.hashCode());
+
+      return result;
+    }
+  }
+}
---
+++ tools/ast_gen.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python
+from pathlib import Path
+from textwrap import dedent, indent
+from functools import reduce
+from collections import namedtuple
+
+Parameter = namedtuple("Parameter", "type name nullable", defaults=["", "", False])
+Token = namedtuple("Token", "name params")
+
+OUT_PATH = "./src/main/java/com/dylmay/JLox/Assets"
+BASE_NAME = "Expr.java"
+INTERFACE_NAME = "Visitor"
+CLASS_NAME = BASE_NAME.split(".")[0]
+PACKAGE_NAME = OUT_PATH.split("java/")[1].replace("/", ".")
+ACCESS_LEVEL = "public"
+IMPORT_LIST = ["javax.annotation.Nullable"]
+
+TOKENS = [
+    Token(
+        "Binary",
+        [
+            Parameter("Expr", "left"),
+            Parameter("Token", "operator"),
+            Parameter("Expr", "right"),
+        ],
+    ),
+    Token(
+        "Ternary",
+        [
+            Parameter("Expr", "condition"),
+            Parameter("Expr", "onTrue"),
+            Parameter("Expr", "onFalse"),
+        ],
+    ),
+    Token("Grouping", [Parameter("Expr", "expression")]),
+    Token("Literal", [Parameter("Object", "value", True)]),
+    Token("Unary", [Parameter("Token", "operator"), Parameter("Expr", "right")]),
+]
+
+
+def create_class(token: Parameter) -> str:
+    def create_params():
+        return ", ".join(
+            map(
+                lambda params: f"{'@Nullable ' if params.nullable else ''}{params.type} {params.name}",
+                token.params,
+            )
+        )
+
+    def create_assignments():
+        return "\n".join(
+            map(lambda assign: f"this.{assign.name} = {assign.name};", token.params)
+        )
+
+    def create_variables():
+        return "\n".join(
+            map(
+                lambda vara: f"{ACCESS_LEVEL} final {'@Nullable ' if vara.nullable else ''}{vara.type} {vara.name};",
+                token.params,
+            )
+        )
+
+    def create_comparison():
+        return (
+            "return "
+            + "\n   && ".join(
+                map(
+                    lambda assign: f"this.{assign.name} != null && this.{assign.name}.equals(i.{assign.name})",
+                    token.params,
+                )
+            )
+            + ";"
+        )
+
+    def create_hashes():
+        return "\n".join(
+            map(
+                lambda assign: f"result = prime * result + (({assign.name} == null) ? 0 : {assign.name}.hashCode());",
+                token.params,
+            )
+        )
+
+    return (
+        dedent(
+            f"""\
+        {ACCESS_LEVEL} static class {token.name} extends {CLASS_NAME} {{
+        %s
+
+          {ACCESS_LEVEL} {token.name}({create_params()}) {{
+        %s
+          }}
+
+          @Override
+          {ACCESS_LEVEL} <R> R accept(Visitor<R> visitor) {{
+            return visitor.visit{token.name}Expr(this);
+          }}
+
+          @Override
+          public boolean equals(@Nullable Object obj) {{
+            if (this == obj) return true;
+
+            if (obj instanceof {token.name} i) {{
+        %s
+            }}
+
+            return false;
+          }}
+
+          @Override
+          public int hashCode() {{
+            final int prime = 31;
+            int result = 1;
+
+        %s
+
+            return result;
+          }}
+        }}"""
+        )
+        % (
+            indent(create_variables(), "  "),
+            indent(create_assignments(), "    "),
+            indent(create_comparison(), "      "),
+            indent(create_hashes(), "    "),
+        )
+    )
+
+
+def create_interface(token) -> str:
+    return f"R visit{token.name}Expr({token.name} expr);"
+
+
+def gen_ast():
+    # base class information
+    interfaces = "\n\n".join(map(lambda clz: create_interface(clz), TOKENS))
+    imports = "\n".join(map(lambda imp: f"import {imp};", IMPORT_LIST))
+    impl = "\n\n".join(map(lambda clz: create_class(clz), TOKENS))
+
+    with open(Path(OUT_PATH) / BASE_NAME, "w", encoding="utf-8") as ast_file:
+        ast_file.write(
+            dedent(
+                f"""\
+            package {PACKAGE_NAME};
+            %s
+            {ACCESS_LEVEL} abstract class {CLASS_NAME} {{
+              {ACCESS_LEVEL} interface {INTERFACE_NAME}<R> {{
+            %s
+              }}
+
+              {ACCESS_LEVEL} abstract <R> R accept(Visitor<R> visitor);
+
+            %s
+            }}
+        """
+            )
+            % (
+                ("\n" + imports + "\n") if len(IMPORT_LIST) > 0 else "",
+                indent(interfaces, "    "),
+                indent(impl, "  "),
+            )
+        )
+
+
+if __name__ == "__main__":
+    gen_ast()
